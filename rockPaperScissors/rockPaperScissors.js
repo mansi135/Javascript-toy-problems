6d8239499783f0c67903fd041cc4eb36b7740549
Mansi Agarwal <mansi.agarwal1410@gmail.com>
2017-12-05 09:54:39 -0800

    completed

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -3,9 +3,9 @@
 * player could throw over a three-round game of rock-paper-scissors.
 *
 * Your output should look something like:
-*   [["rock", "rock", "rock"],
-*    ["rock", "rock", "paper"],
-*    ["rock", "rock", "scissors"],
+*   [["rock", "rock", "rock"],		r-> rr, rp, rs , pr, pp, ps, sr , sp, ss
+*    ["rock", "rock", "paper"],		p-> pr,
+*    ["rock", "rock", "scissors"],	s->	sr,
 *    ["rock", "paper", "rock"],
              ...etc...
      ]
@@ -17,7 +17,27 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
+var rockPaperScissors = function () {
   // TODO: your solution here
+  var finalOutput = [];
+
+  var outputs = ['rock' ,'paper', 'scissors'];
+  
+
+
+	for(var i = 0; i < 3; i++) {
+		for (var j =0; j<3;j++) {
+			for(var k=0; k<3; k++) {
+				var arr = [];
+				arr.push(outputs[i]);
+				arr.push(outputs[j]);
+				arr.push(outputs[k]);
+				finalOutput.push(arr);
+			}
+		}
+	}
+
+
+  return finalOutput;
+
 };
